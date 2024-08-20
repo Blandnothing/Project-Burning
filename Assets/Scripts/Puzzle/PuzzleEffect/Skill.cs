cc4f8e414b29af21e40fdb9c175d59c7e60041f8
Bland_nothing <2067721994@qq.com>
2024-08-20 23:07:46 +0800

    [fix](level) item collector

--- Assets/Scripts/Puzzle/PuzzleEffect/Skill.cs
+++ Assets/Scripts/Puzzle/PuzzleEffect/Skill.cs
@@ -1,9 +1,12 @@
+using Newtonsoft.Json;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[JsonObject(MemberSerialization.OptOut)]
 public abstract class Skill : PuzzleEffect
 {
+    [JsonIgnore]
     public GameObject vfx;
     abstract public void AddSkill(KeyCode key);
     virtual public void ChangeValue(float v1)
